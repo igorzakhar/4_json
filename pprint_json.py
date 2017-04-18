068977b5a4e1ff85eac4cba35f955df8c6646da9
igorzakhar <igorzakhar@gmail.com>
2017-04-18 19:26:58 +0000

    Add imports at the top of the file

--- pprint_json.py
+++ pprint_json.py
@@ -1,28 +1,27 @@
 import json
+import sys
+import os
 
 
 def load_data(filepath):
     with open(filepath) as file:
-        data = json.load(file)
-    return data
+        json_data = json.load(file)
+    return json_data
     
 
 def pretty_print_json(data):
-    output = json.dumps(data, ensure_ascii=False, 
-                        sort_key=True, indent=4)
-    return output
+    formatted_json = json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4)
+
+    return formatted_json
 
 
 if __name__ == '__main__':
-    import sys
-    import os
-
 
     if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
         filepath = sys.argv[1]
-        data = load_data(filepath)        
-        output = pretty_print_json(data)
-        print(output)
+        json_data = load_data(filepath)        
+        formatted_json = pretty_print_json(json_data)
+        print(formatted_json)
     else:
         print("Enter the filename.")
         print("Example: python pprint_json.py <filename>")
